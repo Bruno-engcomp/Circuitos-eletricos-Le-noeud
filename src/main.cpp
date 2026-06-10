5a4d61f41b3be94efbc9904c248e5b57e15f2eb4
bruno22007 <brunoalmeida22007@gmail.com>
2026-06-10 18:38:48 -0300

    Pasta nova para dos arquivos .cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,9 @@
-// #include <ESP32Servo.h>
-//
-// Servo servos[6];
-//
-// int pinos[6] = {13, 12, 14, 27, 26 ,25};
-//
-// //Algumas letras so para testar
-//
-// //Letra A
-// int letraA[6] = {1,0,0,0,0,0};
-//
-// //Letra B
-// int letraB[6] = {1,1,0,0,0,0};
-//
-//
-//
-//
-// void setup() {
-//   for (int i = 0; i < 6; i++) {
-//     servos[i].attach(pinos[i]); //Attach the Servo variable to a pin.
-//   }
-// }
-//
-// void loop() {
-//   mostrarLetra(letraA);
-//   delay(2000);
-//
-//   mostrarLetra(letraB);
-//   delay(2000);
-// }
-//
-// void mostrarLetra(int letra[6])
-// {
-//   for(int i = 0; i < 6; i++)
-//   {
-//     if(letra[i] == 1)
-//       servos[i].write(50);
-//     else
-//       servos[i].write(0);
-//   }
-// }
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("ESP32 funcionando!");
+}
+
+void loop() {
+}
